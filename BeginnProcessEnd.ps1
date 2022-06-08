35294bbf6d7ea7f5374ebbc279ab068c042ebd71
Stefan Ober <StefanO@ppedv.de>
2022-06-08 14:40:40 +0200

    Beginn ProcessEnd

---
+++ BeginnProcessEnd.ps1
@@ -0,0 +1,21 @@
+﻿[cmdletBinding()]
+param(
+[Parameter(ValueFromPipeLine=$true,ValueFromPipeLineByPropertyName,Mandatory=$true)]
+[string]$Name
+)
+
+Begin
+{
+    Write-Host -Object "Wird einmalig ausgeführt" -BackgroundColor White -ForegroundColor Blue
+}
+Process
+{# Wird für jedes OBjekt ausgeführt
+    Write-Host -ForegroundColor (Get-Random -Maximum 15 -Minimum 0) -Object $Name
+}
+End
+{
+     Write-Host -Object "Wird einmalig ausgeführt" -BackgroundColor White -ForegroundColor Blue
+}
+
+
+
